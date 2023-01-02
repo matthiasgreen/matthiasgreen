4010210a27e8388577f4465fb9036249edf89906
matthiasgreen <98462622+matthiasgreen@users.noreply.github.com>
2023-01-02 14:48:12 +0100

    Add files via upload

--- codeqcm.js
+++ codeqcm.js
@@ -1,9 +1,15 @@
+function atLeastOneRadio(i) {
+    return (document.querySelector('input[name="q'+i.toString()+'"]:checked') != null)
+}
+
 function UpdateOutput(event){
     let nbQuestions=17
     let scoreCoachs = [0, 0, 0, 0, 0] //0 : éole, 1 : matt,  2 : océ, 3 : sach, 4 : personne
     
     for (let i = 1; i<=nbQuestions; i++){
-        scoreCoachs[document.querySelector('input[name="q'+i.toString()+'"]:checked').value]+=1
+        if (atLeastOneRadio(i)){
+            scoreCoachs[document.querySelector('input[name="q'+i.toString()+'"]:checked').value]+=1
+        }
     }
     let bestCoach = scoreCoachs.indexOf(Math.max(...scoreCoachs));
     console.log(bestCoach)
@@ -28,5 +34,10 @@ function Pauvre(event){
     alert("Malheureusement votre budget est trop faible, nous vous conseillons de vous rediriger vers un coach en finance")
 }
 
+function Malandrin(event){
+    window.location.href = "https://fr.wiktionary.org/wiki/malandrin";
+}
+
 document.getElementById("calc").addEventListener("click", UpdateOutput)
 document.getElementById("pauvre").addEventListener("click", Pauvre)
+document.getElementById("malandrin").addEventListener("click", Malandrin)
